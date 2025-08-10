a0c02f22060be914c72e5c3df9bf80fe81405bb3
tiogars <tiogars@gmail.com>
2025-08-11 00:17:58 +0200

    Refactor App class documentation and constructor for clarity

--- src/main/java/fr/tiogars/myentity/App.java
+++ src/main/java/fr/tiogars/myentity/App.java
@@ -1,11 +1,22 @@
 package fr.tiogars.myentity;
 
 /**
- * Hello world!
- *
+ * Main application class.
  */
-public class App 
+public class App
 {
+    /**
+     * Private constructor to prevent instantiation
+     */
+    private App()
+    {
+        // Prevent instantiation
+    }
+
+    /**
+     * Main method to run the application.
+     * @param args Command line arguments
+     */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
